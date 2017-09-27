480caca24ecc2daca401883a6eb669c6d446520b
Luis de la Rosa <luis@luisdelarosa.com>
2017-09-27 00:04:11 -0400

    Ensure that both LEDs are turned off before exiting via Control-C via the BlueSignals module.

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "doubleblink",
     dependencies: [
         .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0),
+	.Package(url: "https://github.com/IBM-Swift/BlueSignals.git", majorVersion: 0)
     ]
 )
--- Sources/Main.swift
+++ Sources/Main.swift
@@ -1,5 +1,7 @@
 import SwiftyGPIO
 import Glibc
+// why isn't it called BlueSignals?
+import Signals
  
 let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPi3)
 var pinA = gpios[.P17]!
@@ -9,6 +11,15 @@ pinB.direction = .OUT
 pinA.value = 0
 pinB.value = 1 
  
+Signals.trap(signal: .int) { signal in
+	// Ensure we turn off both LEDs before exiting
+	pinA.value = 0
+	pinB.value = 0
+
+	// Leave the program, otherwise you effectively disable Control-C
+	exit(0)
+}
+
 repeat {
 	pinA.value = (pinA.value == 0) ? 1 : 0
 	pinB.value = (pinB.value == 0) ? 1 : 0
